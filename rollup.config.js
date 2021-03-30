b176a8d70fa0cc11f855439dd47bdc0354fdf1d7
rmhari <rm.haripriya@gmail.com>
2021-03-30 08:35:40 +0530

    initial setup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+export default [{
+  input: 'src/js/main/index.js',
+  output: {
+    file: 'dist/js/index.js',
+    format: 'cjs'
+  }
+},
+{
+  input: 'src/js/sql-practice/index.js',
+  output: {
+    file: 'dist/js/sql-practice.js',
+    format: 'cjs'
+  }}];
+
